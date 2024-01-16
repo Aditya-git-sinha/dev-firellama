e44ef7b6314d3ad75dd1b7e5a4966c217c815f00
Aditya <adityakumar113141@gmail.com>
2024-01-16 13:25:44 +0530

    Second Container Working just fine

--- src/components/DustAnimation.js
+++ src/components/DustAnimation.js
@@ -1,138 +1,159 @@
 import React, { useRef, useEffect } from 'react';
 
+const canvasStyles = {
+  display: 'block',
+  background: '#FFF',
+};
+
 const DustParticles = (props) => {
-    const canvasRef = useRef(null);
-    const particlesArray = useRef([]);
+  console.log('init')
+  const canvasRef = useRef(null);
+  let image = new Image();
+  let particlesArray = [];
+  const mouse = {
+    x: null,
+    y: null,
+    radius: 100
+  };
 
-    const mouse = {
-        x: null,
-        y: null,
-        radius: 100
-    };
-
-    class Particle {
-        constructor(x, y, color) {
-            this.x = x;
-            this.y = y;
-            this.size = 1.5;
-            this.baseX = this.x;
-            this.baseY = this.y;
-            this.density = (Math.random() * 30) + 1;
-            this.color = color;
-        }
-
-        draw(ctx) {
-            ctx.fillStyle = this.color;
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-            ctx.closePath();
-            ctx.fill();
-        }
-
-        update(ctx) {
-            let dx = mouse.x - this.x;
-            let dy = mouse.y - this.y;
-            let distance = Math.sqrt(dx * dx + dy * dy);
-            let forceDirectionX = dx / distance;
-            let forceDirectionY = dy / distance;
-            let maxDistance = mouse.radius;
-            let force = (maxDistance - distance) / maxDistance;
-            let directionX = forceDirectionX * force * this.density * 0.6;
-            let directionY = forceDirectionY * force * this.density * 0.6;
-
-            if (distance < maxDistance) {
-                this.x += directionX;
-                this.y += directionY;
-            } else {
-                let deltaX = this.x - this.baseX;
-                let deltaY = this.y - this.baseY;
-                let distanceBase = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-                let baseForce = distanceBase * this.density * 0.02;
-
-                if (distanceBase > 1) {
-                    this.x -= deltaX / distanceBase * baseForce;
-                    this.y -= deltaY / distanceBase * baseForce;
-                } else {
-                    this.x = this.baseX;
-                    this.y = this.baseY;
-                }
-            }
-        }
+  class Particle {
+    constructor(x, y, color) {
+      this.x = x;
+      this.y = y;
+      this.size = 1.5;
+      this.baseX = this.x;
+      this.baseY = this.y;
+      this.density = (Math.random() * 30) + 1;
+      this.color = color;
     }
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
+    draw(ctx) {
+      ctx.fillStyle = this.color;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fill();
+    }
 
-        function initParticlesFromImage(imageData) {
-            const image = new Image();
-            image.src = `data:image/png;base64,${imageData}`;
-            image.onload = () => {
-                ctx.drawImage(image, 0, 0);
-                const pixelData = ctx.getImageData(0, 0, image.width, image.height);
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+    update() {
+      let dx = mouse.x - this.x;
+      let dy = mouse.y - this.y;
+      let distance = Math.sqrt(dx * dx + dy * dy);
+      let forceDirectionX = dx / distance;
+      let forceDirectionY = dy / distance;
+      let maxDistance = mouse.radius;
+      let force = (maxDistance - distance) / maxDistance;
+      let directionX = forceDirectionX * force * this.density * 0.6;
+      let directionY = forceDirectionY * force * this.density * 0.6;
 
-                for (let y = 0; y < pixelData.height; y += 4) {
-                    for (let x = 0; x < pixelData.width; x += 4) {
-                        const i = (y * 4 * pixelData.width) + (x * 4);
-                        if (pixelData.data[i + 3] > 128) {
-                            const red = pixelData.data[i];
-                            const green = pixelData.data[i + 1];
-                            const blue = pixelData.data[i + 2];
-                            const color = `rgba(${red},${green},${blue})`;
-                            particlesArray.current.push(new Particle(x, y, color));
-                        }
-                    }
-                }
-                animate();
-            };
+      if (distance < maxDistance) {
+        this.x += directionX;
+        this.y += directionY;
+      } else {
+        let deltaX = this.x - this.baseX;
+        let deltaY = this.y - this.baseY;
+        let distanceBase = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        let baseForce = distanceBase * this.density * 0.02;
+
+        if (distanceBase > 1) {
+          this.x -= deltaX / distanceBase * baseForce;
+          this.y -= deltaY / distanceBase * baseForce;
+        } else {
+          this.x = this.baseX;
+          this.y = this.baseY;
         }
+      }
+    }
+  }
 
-        const handleMouseMove = (event) => {
-            mouse.x = event.x;
-            mouse.y = event.y;
-        };
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
 
-        const handleMouseOut = () => {
-            mouse.x = undefined;
-            mouse.y = undefined;
-        };
+    const resizeCanvas = () => {
+      if (props.encodedImageData && image.complete) {
+        console.log('damn')
+        initParticlesFromImage();
+      }
+    }
 
-        const animate = () => {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            for (let i = 0; i < particlesArray.current.length; i++) {
-                particlesArray.current[i].draw(ctx);
-                particlesArray.current[i].update(ctx);
-            }
-            requestAnimationFrame(animate);
-        };
+    let animationFrameId;
 
-        window.addEventListener('mousemove', handleMouseMove);
-        window.addEventListener('mouseout', handleMouseOut);
+    const animate = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      particlesArray.forEach(particle => {
+        particle.draw(ctx);
+        particle.update(ctx);
+      });
+      animationFrameId = requestAnimationFrame(animate);
+    };
 
-        const handleResize = () => {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-            particlesArray.current = [];
-            initParticlesFromImage(props.encodedImageData);
-        };
+    const createParticlesFromData = (pixelData) => {
+      particlesArray = [];
+      for (let y = 0; y < pixelData.height; y += 4) {
+        for (let x = 0; x < pixelData.width; x += 4) {
+          const i = (y * 4 * pixelData.width) + (x * 4);
+          if (pixelData.data[i + 3] > 128) {
+            const red = pixelData.data[i];
+            const green = pixelData.data[i + 1];
+            const blue = pixelData.data[i + 2];
+            const color = `rgba(${red},${green},${blue})`;
+            particlesArray.push(new Particle(x, y, color));
+          }
+        }
+      }
+    }
 
-        window.addEventListener('resize', handleResize);
+    const initParticlesFromImage = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+      console.log(image.width, image.height, "fck")
+      if (image.width > 0 && image.height > 0) {
+        ctx.drawImage(image, 0, 0);
+        const pixelData = ctx.getImageData(0, 0, image.width, image.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        createParticlesFromData(pixelData);
+        animate();
+      }
 
-        initParticlesFromImage(props.encodedImageData);
+    }
 
-        // Cleanup event listeners on unmount
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-            window.removeEventListener('mouseout', handleMouseOut);
-            window.removeEventListener('resize', handleResize);
-        };
+    window.addEventListener('resize', resizeCanvas);
 
-    }, [props.encodedImageData]);
+    const initParticles = () => {
+      image.src = `data:image/png;base64,${props.encodedImageData.trim()}`;
+      console.log(image.height, image.width, "meow")
+      image.onload = initParticlesFromImage
+    };
 
-    return <canvas ref={canvasRef} className={props.className} style={{ display: 'block', background: '#FFF' }} />;
+    initParticles();
+
+    window.addEventListener('mousemove', (event) => {
+      mouse.x = event.clientX;
+      mouse.y = event.clientY;
+    });
+
+    window.addEventListener('mouseout', () => {
+      mouse.x = undefined;
+      mouse.y = undefined;
+    });
+
+    // Cleanup
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+      window.removeEventListener('mousemove', (event) => {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+      });
+      window.removeEventListener('mouseout', () => {
+        mouse.x = undefined
+        mouse.y = undefined;
+      });
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, [props.encodedImageData]); // Only re-run the effect if new imageData comes in
+
+  return <canvas ref={canvasRef} style={canvasStyles} />;
 };
 
 export default DustParticles;
