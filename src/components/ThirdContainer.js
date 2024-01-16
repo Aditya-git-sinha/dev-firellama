42d97612e3f4e645985dcd8113122022abedd8e2
Aditya <adityakumar113141@gmail.com>
2024-01-16 23:11:42 +0530

    Third Container beta
    Need to add prev project details
    
    Multiple Images Inside Carousel in Dialog

--- src/components/ThirdContainer.js
+++ src/components/ThirdContainer.js
@@ -7,29 +7,33 @@ import '../css/ThirdContainer.css';
 const ThirdContainer = ({ openDialog }) => {
   return (
     <div className="third-container">
-        {previousProjectsData.map((project) => (
-            <div key={project.title} className="project-box">
-                <img src={project.pictureLocation} alt={project.title} className="project-image" />
-                <div className="small-description">{project.smallDescription}</div>
-                <button onClick={() => openDialog(project)} className="view-more-btn">View More</button>
-            </div>
-        ))}
+      <h2 className="section-heading">Previous Projects</h2>
 
-        <div className="project-box megsoft-product">
-            <SmallLaptopComponent iframeSrc={megsoft} />
-            <div className="megsoft-text">
-                <h1>MegSoft - Business Accounting Software</h1>
-                <div className="description-background">
-                    <p className="megsoft-description">Streamline your accounting processes and take control of your business finances with our user-friendly software.</p>
-                </div>
-                <button
-                  name="submit"
-                  className="btn-2" // Updated class name to match the style provided
-                >
-                  Coming Soon
-                </button>
-            </div>
+      {previousProjectsData.map((project) => (
+        <div key={project.title} className="previous-project">
+          <img src={project.pictureLocation} alt={project.title} className="project-image" />
+          <div className="small-description">{project.smallDescription}</div>
+          <button onClick={() => openDialog(project)} className="view-more-btn">View More</button>
         </div>
+      ))}
+
+      <h2 className="section-heading">Upcoming Projects</h2>
+
+      <div className="project-box megsoft-product">
+        <SmallLaptopComponent iframeSrc={megsoft} />
+        <div className="megsoft-text">
+          <h1>MegSoft - Business Accounting Software</h1>
+          <div className="description-background">
+            <p className="megsoft-description">Streamline your accounting processes and take control of your business finances with our user-friendly software.</p>
+          </div>
+          <button
+            name="submit"
+            className="btn-2" // Updated class name to match the style provided
+          >
+            Coming Soon
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
